555032f12aff308e988ef8b9b02f01bfa7f3cc6b
Chris Ditfort <chrisditfort@outlook.com>
2023-09-26 23:54:58 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+window = tk.Tk()
+window.title('Getting and setting widgets')
+
+label = ttk.Label(master=window,text='Label')
+label.pack()
+
+entry = ttk.Entry(master=window)
+entry.pack()
+
+def button_func():
+    label.config(text=entry.get())
+    entry.config(state='disabled')
+button = ttk.Button(master=window,text='Button',command=button_func)
+button.pack()
+def button2():
+    label.config(text='some other text')
+    entry.config(state='enabled')
+button2 = ttk.Button(master=window, text='Button 2', command =  button2)
+button2.pack()
+
+window.mainloop()
